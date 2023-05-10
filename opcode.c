5274f6acba738b065d10f22b9c3b60604f5f7168
Holberton <holberton@Holbertons-iMac.local>
2023-05-10 10:38:23 -0500

    project start

---
+++ opcode.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+ 
+void myPush(stack_t **doubly, unsigned int cline)
+{
+	int n, j;
+
+	if (!vglo.arg)
+	{
+		print(2, "L%u: ", cline);
+		print(2, "usage: push integer\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	for (j = 0; vglo.arg[j] != '\0'; j++)
+	{
+		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
+		{
+			print(2, "L%u: ", cline);
+			print(2, "usage: push integer\n");
+			free_vglo();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	n = atoi(vglo.arg);
+
+	if (vglo.lifo == 1)
+		add_dnodeint(doubly, n);
+	else
+		add_dnodeint_end(doubly, n);
+}
+
+ 
+void myPall(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+	(void)cline;
+
+	aux = *doubly;
+
+	while (aux)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+	}
+}
+
+ 
