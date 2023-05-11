a92302ae89f2a546e651a3d8c4bb5ccdf4abfaa2
Holberton <holberton@Holbertons-iMac.local>
2023-05-11 11:43:45 -0500

    correction fn

--- get_opcodes.c
+++ get_opcodes.c
@@ -13,7 +13,7 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"push", push},
 		{"pall", pall},
 		{"pint", pint},
-		{"pop", _pop},
+		{"pop", pop},
 		{"swap", _swap},
 		{"queue", _queue},
 		{"stack", _stack},
