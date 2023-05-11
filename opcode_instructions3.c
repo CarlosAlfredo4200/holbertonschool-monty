b4345b0297034088c9cff06cdc42d1f305286c78
Holberton <holberton@Holbertons-iMac.local>
2023-05-11 09:52:12 -0500

    add fn pop swap

--- opcode_instructions3.c
+++ opcode_instructions3.c
@@ -99,7 +99,7 @@ void _mod(stack_t **doubly, unsigned int cline)
 
 	aux = (*doubly)->next;
 	aux->n %= (*doubly)->n;
-	_pop(doubly, cline);
+	pop(doubly, cline);
 }
 /**
  * _pchar - print the char value of the first element
