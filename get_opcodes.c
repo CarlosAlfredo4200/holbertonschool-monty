99451e0afb1311023131d2d24377ab95bab17648
Holberton <holberton@Holbertons-iMac.local>
2023-05-11 09:32:16 -0500

    x

--- get_opcodes.c
+++ get_opcodes.c
@@ -11,7 +11,7 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 {
 	instruction_t instruct[] = {
 		{"push", push},
-		{"pall", _pall},
+		{"pall", pall},
 		{"pint", _pint},
 		{"pop", _pop},
 		{"swap", _swap},
